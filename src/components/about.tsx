6d81b99288a0bba46ccd84f3e37d80d0ddf3dc82
MIFZALAQIL\mifza <mifzal.aqil@gmail.com>
2025-09-24 20:35:29 +0800

    fix about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,7 +6,7 @@ export default function About() {
       <h2 className="text-lg md:text-3xl font-light mb-12 text-center tracking-wider">ABOUT</h2>
 
       <p className="text-sm md:text-base text-muted-foreground leading-relaxed">
-        Mifzal, 23, is an emerging composer blending emotion and innovation into Progressive and Cinematic contemporary instrumental music. With eight years of classical guitar mastery and experience in music production, he creates compositions that captivate and inspire.
+        Mifzal, 23, is an emerging composer blending emotion and innovation into Progressive and Cinematic contemporary instrumental music. With a combined ten years of classical guitar mastery and experience in music production, he creates compositions that captivate and inspire.
 
         His releases showcase intricate sound design and immersive storytelling, and with upcoming projects on the horizon, Mifzal continues to push the boundaries of instrumental music.
       </p>
